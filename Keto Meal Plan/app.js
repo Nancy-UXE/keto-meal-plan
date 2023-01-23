d0e2b52809a743784c75f6887ada687990a08729
Nancy Siam <111718786+Nancy-UXE@users.noreply.github.com>
2023-01-22 18:11:37 -0800

    Initial commit for Keto Meal Plan Project

---
+++ Keto Meal Plan/app.js
@@ -0,0 +1,15 @@
+const menu = [
+  {
+    id: 1,
+    title: "meal 1",
+    category: "breakfast",
+    calories: 200,
+    img: "./images/item-1.jpeg",
+    desc: ` `,
+  },
+];
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {});
